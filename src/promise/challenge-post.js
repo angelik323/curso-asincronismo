ce1f3cc7ca6ffdc66064aeb0696f620c8601d6fa
angelik323 <angelica.loaiza323@gmail.com>
2023-02-07 15:32:17 -0500

    creación de nuevos productos en el API

---
+++ src/promise/challenge-post.js
@@ -0,0 +1,29 @@
+//Guardar datos en nuestra API
+//Creación de nuevos productos
+import fetch from "node-fetch";
+const API = 'https://api.escuelajs.co/api/v1';
+
+function postData(urlApi, data) {
+    const response = fetch(urlApi, {
+        method: 'POST',
+        mode: 'cors',
+        credencials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+    return response;
+}
+
+const data = {
+    "title": "New computer today",
+    "price": 10000000,
+    "description": "A description",
+    "categoryId": 1,
+    "images": ["https://placeimg.com/640/480/any"]
+}
+
+postData(`${API}/products`, data)
+    .then(response => response.json())
+    .then(data => console.log(data))
